564872ea1a56de1966c74942eadca406fc7b06d9
kakapa <molotsipilane@gmail.com>
2025-04-24 15:39:46 +0200

    Add DashboardController & HomeController

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,22 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Middleware\MustVerifyMobileNumber;
 use App\Http\Middleware\UpdateProfileOfNewlyRegisteredUser;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+Route::name('guest')->group(function () {
+    Route::get('/', [HomeController::class, 'showHome']);
+    Route::get('/about', [HomeController::class, 'showAbout']);
+    Route::get('/contact', [HomeController::class, 'showContact']);
+    Route::get('/resume', [HomeController::class, 'showResume']);
 });
-
-Route::inertia('/about', 'About')->name('about');
-
-Route::inertia('/contact', 'Contact')->name('contact');
-
-Route::inertia('/resume', 'Resume', [
-    'name' => 'Molotsi Paul Pilane'
-])->name('resume');
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware(['auth', MustVerifyMobileNumber::class, 'verified', UpdateProfileOfNewlyRegisteredUser::class])
     ->group(function () {
-        Route::inertia('/dashboard', 'Dashboard')->name('dashboard');
+        Route::get('/dashboard', [DashboardController::class, 'showDashboard'])->name('dashboard');
     });
 
 Route::middleware('auth')->group(function () {
